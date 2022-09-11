6a391986154cf5153815d817cf688ff9ce8294c5
stevensamir20 <steven.samir@eng.suez.edu.eg>
2022-09-11 13:16:49 +0200

    initial

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,28 +12,28 @@ export class HeaderComponent implements OnInit {
   logoutbtn:boolean;
 
   constructor(private dataService: LoginService) {
-      dataService.getLoggedInName.subscribe(name => this.changeName(name));
-      if(this.dataService.isLoggedIn()){
-          console.log("loggedin");
-          this.loginbtn=false;
-          this.logoutbtn=true
-      }
-      else{
-          this.loginbtn=true;
-          this.logoutbtn=false
-      }
+    dataService.getLoggedInName.subscribe(name => this.changeName(name));
+    if(this.dataService.isLoggedIn()){
+        console.log("loggedin");
+        this.loginbtn=false;
+        this.logoutbtn=true
+    }
+    else{
+        this.loginbtn=true;
+        this.logoutbtn=false
+    }
   }  
 
   ngOnInit() {}
 
   private changeName(name: boolean): void {
-      this.logoutbtn = name;
-      this.loginbtn = !name;
+    this.logoutbtn = name;
+    this.loginbtn = !name;
   }
 
   logOut() {
-      this.dataService.deleteToken();
-      window.location.href = window.location.href;
+    this.dataService.deleteToken();
+    window.location.href = window.location.href;
   }
   
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -15,8 +15,8 @@ export class LoginComponent implements OnInit {
 
   constructor(private fb: FormBuilder, private dataService: LoginService, private router: Router) {
     this.loginForm = this.fb.group({
-        email: ['', [ Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
-        password: ['', Validators.required]
+      email: ['', Validators.compose([Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(8)])]
     });
   }
 
@@ -33,4 +33,8 @@ export class LoginComponent implements OnInit {
         (error) => { alert('Error: ' + error) }
     );
   }
+
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
+  get fc() { return this.loginForm.controls }
 }
