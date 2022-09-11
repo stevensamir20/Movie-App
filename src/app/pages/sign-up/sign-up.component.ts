eaa4999a035d703bb4448f5870d86fbe935b401f
stevensamir20 <steven.samir@eng.suez.edu.eg>
2022-09-11 13:16:37 +0200

    initial

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -15,9 +15,12 @@ export class SignUpComponent implements OnInit {
 
   constructor(private fb: FormBuilder, private dataService: LoginService, private router: Router) {
       this.registerForm = this.fb.group({
-          email: ['', Validators.required],
-          password: ['', Validators.required],
-          name: ['', Validators.required]
+        name: ['', Validators.compose([Validators.required, Validators.pattern("^(?=.{4,25}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$")])],
+        email: ['', Validators.compose([Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])],
+        password: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+        confirmPassword: ['', [Validators.required]], 
+      }, {
+        validators: this.passMatch('password', 'confirmPassword')
       });
   }
 
@@ -27,13 +30,33 @@ export class SignUpComponent implements OnInit {
       this.dataService.userRegister(regForm.value.name, regForm.value.email, regForm.value.password)
       .pipe(first())
       .subscribe(
-          data => { this.router.navigate(['login']); },
+          data => { 
+            alert(data);
+            this.router.navigate(['login']); },
           error => {}
       );
   }
 
-  // get email() { return this.registerForm.get('email'); }
-  // get password() { return this.registerForm.get('password'); }
-  // get name() { return this.registerForm.get('name'); }
+  get name() { return this.registerForm.get('name'); }
+  get email() { return this.registerForm.get('email'); }
+  get password() { return this.registerForm.get('password'); }
+  get confirmPassword() { return this.registerForm.get('confirmPassword'); }
+  get fc() { return this.registerForm.controls }
+
+  passMatch(password: string, confirmPassword: string) {
+    return (formGroup: FormGroup) => {
+
+      const passControl = formGroup.controls[password]
+      const confPassControl = formGroup.controls[confirmPassword]
+
+      if (confPassControl.errors && !confPassControl.errors['passMatch']){
+        return;
+      }
+
+      if (passControl.value !== confPassControl.value) {
+        confPassControl.setErrors({ passMatch: true})
+      } else { confPassControl.setErrors(null) }
+    }
+  }
 
 }
