33cabe8c2d6a4bcf5de26f2a77022081b2337bb2
stevensamir20 <steven.samir@eng.suez.edu.eg>
2022-09-14 21:55:16 +0200

    final

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { FavoritesService } from 'src/app/shared/services/favorites.service';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
-import { faEye, faStar, faSpinner, faRemove } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faStar, faSpinner, faRemove, faClose } from '@fortawesome/free-solid-svg-icons';
 import { LoginService } from 'src/app/shared/services/login.service';
 import { MoviesPayload } from 'src/app/shared/interfaces/movies-payload';
 
@@ -15,20 +15,19 @@ import { MoviesPayload } from 'src/app/shared/interfaces/movies-payload';
 export class FavoritesComponent implements OnInit, OnDestroy {
 
   showLoading: boolean = false;
-  favorites?: any[];
+  favorites?: MoviesPayload[];
   sub?: Subscription;
   errorMsg?: string;
   userData: any;
+  favMsg?: string;
 
   constructor(
     private favoriteService: FavoritesService, 
     private loginService: LoginService) 
-  {
-    this.userData = this.loginService.getUserObj();
-    // this.favorites = this.favoriteService.getFavorites()
-  }
+  { this.userData = this.loginService.getUserObj(); }
 
   ngOnInit(): void {
+    this.showLoading = true;
     this.sub = this.favoriteService.getFavorites(this.userData.id).subscribe(
       (res) => {
         this.favorites = res;
@@ -36,21 +35,27 @@ export class FavoritesComponent implements OnInit, OnDestroy {
       },
       (error) => { 
         this.showLoading = false;
-        console.log(error);
         this.errorMsg = "Couldn't get favorites, try again later!";
       }
     );
   }
 
   deleteMovie(movieId: number){
+    console.log(this.favorites);
+    
+    this.favMsg = "Deleting from favorites.."
     this.favoriteService.deleteFromFavorites(this.userData.id, movieId).subscribe(
       (res) => {
-       console.log(res);
+        console.log(this.favorites);
+        let movieDeleted = this.favorites?.findIndex(object => { return object.movieId === movieId; });
+        if (movieDeleted){  this.favorites?.splice(movieDeleted, 1); }
+        this.favMsg = "Deleted from favorites!"
+        console.log(this.favorites);
       },
-      (error) => { 
-        console.log(error);
+      (error) => {
+        this.favMsg = "Couldn't delete from favorites, try again later!"
       }
-    );
+    )
   }
   
   ngOnDestroy(): void {
@@ -62,4 +67,5 @@ export class FavoritesComponent implements OnInit, OnDestroy {
   faSpinner = faSpinner;
   faStar = faStar;
   faDelete = faRemove;
+  faClose = faClose;
 }
